3b1264a48ddcbb11fb81f7570005078599b24ffa
mjc <1119250897@qq.com>
2018-12-21 18:39:49 +0800

    框架整合了redis， 仍然需要自己 封装redis操作， 仍然需要自己 封装缓存。

---
+++ src/main/java/mjc/myutil/RedisCaheUtils.java
@@ -0,0 +1,8 @@
+package mjc.myutil;
+
+/**
+ * Created by gcb on 2018/12/21.
+ * 用于封装对RedisCahe的缓存
+ */
+public class RedisCaheUtils {
+}
---
+++ src/main/java/mjc/myutil/RedisUtils.java
@@ -0,0 +1,8 @@
+package mjc.myutil;
+
+/**
+ * Created by gcb on 2018/12/21.
+ * 用来自己封装 redisTemple 操作redis的增删改查
+ */
+public class RedisUtils {
+}
