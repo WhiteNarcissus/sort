e3d50875c64068d26deee2132d2f95e99108b2ab
mjc <1119250897@qq.com>
2019-03-19 17:42:31 +0800

    观察者模式

--- src/main/java/mode/simplFactory/SimpleFactory.java
+++ src/main/java/mode/simplFactory/SimpleFactory.java
@@ -3,7 +3,7 @@ package mode.simplFactory;
 
 /**
  * Created by gcb on 2018/12/20.
- * 简单工厂模式 ，将类的创建 交个 SimpleFactory 要什么类就 写一个case，解耦类的创建 （spring 干的就是这个事情，mybatis 将sql解耦 为 mapper 文件，以及Result 解耦）
+ * 简单工厂模式 ，将类的创建 交个 SimpleFactory 要什么类就 写一个case，解耦类的创建 （spring 干的就是这个事情，mybatis 将sql解耦 为 mjc.dao.mapper 文件，以及Result 解耦）
  * ps： springBoot 则将 各种配置文件 解耦为注解
  * 但是这样的话依然还是 无法做到真正地解耦 ，还得依赖于case
  */
